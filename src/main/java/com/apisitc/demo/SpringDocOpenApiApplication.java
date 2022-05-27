6e4dc498beff343b55a63197dd7ccb782f0620e2
ApisitChu <apisitc@generali.co.th>
2022-05-27 09:46:09 +0700

    initial

---
+++ src/main/java/com/apisitc/demo/SpringDocOpenApiApplication.java
@@ -0,0 +1,14 @@
+package com.apisitc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocOpenApiApplication {
+
+	//http://localhost:8080/swagger-ui/index.html
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocOpenApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/apisitc/demo/example/model/package-info.java
@@ -0,0 +1,1 @@
+package com.apisitc.demo.example.model;
